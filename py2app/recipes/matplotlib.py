9f5a014faf4394d6b3aada94561959dad0bb1c6d
Ronald Oussoren <ronaldoussoren@mac.com>
2010-07-13 08:33:34 +0000

    Fix issue in matplotlib recipe as mentioned on pythonmac-sig list.

--- py2app/recipes/matplotlib.py
+++ py2app/recipes/matplotlib.py
@@ -2,7 +2,11 @@ def check(cmd, mf):
     m = mf.findNode('matplotlib')
     if m is None or m.filename is None:
         return None
-    mf.import_hook('pytz.zoneinfo', m, ['UTC'])
+
+    # XXX: unclear why this is needed and according
+    # to mail on pythonmac-sig this breaks with
+    # current versions of matplatlib and pytz.
+    #mf.import_hook('pytz.zoneinfo', m, ['UTC'])
     return dict(
         packages = ['matplotlib']
     )
