49d1bbd8d6ea801df1fe55a915117d5f64752214
Ronald Oussoren <ronaldoussoren@mac.com>
2010-08-07 13:50:25 +0000

    Remove a reference to the MacPython website from the error dialog that is displayed when an application terminates with an uncaught exception.

--- py2app/apptemplate/lib/__error__.sh
+++ py2app/apptemplate/lib/__error__.sh
@@ -11,6 +11,9 @@ if ( test -n "$2" ) ; then
 	echo "See the Console for a detailed traceback."
 else
 	echo "$1 Error"
-	echo "MacPython 2.3 is required to run this application.";
-	echo "ERRORURL: http://homepages.cwi.nl/~jack/macpython/index.html Visit the MacPython Website";
+
+	# Usage: ERRORURL <anURL> <a button label>, this is used by the 
+	# bundle runner to put up a dialog.
+	#echo "ERRORURL: http://www.python.org/ Visit the Python Website
+# echo "ERRORURL: http://homepages.cwi.nl/~jack/macpython/index.html Visit the MacPython Website"
 fi
