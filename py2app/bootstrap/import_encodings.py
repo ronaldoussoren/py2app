71fd5b28e91ec8fbe40f9d12d494c53bba21995f
Ronald Oussoren <ronaldoussoren@mac.com>
2011-01-19 23:59:43 +0100

    Add missing file....

---
+++ py2app/bootstrap/import_encodings.py
@@ -0,0 +1,23 @@
+def _import_encodings():
+    import os
+    import imp
+    import encodings
+    import pkgutil
+    import sys
+    del sys.path[:2]
+    import encodings.aliases
+
+    encodings.__path__ = pkgutil.extend_path(
+            encodings.__path__,
+            encodings.__name__)
+    imp.reload(encodings)
+
+    import encodings.mac_roman
+    encodings.aliases.__file__ = os.path.join(
+            os.path.dirname(encodings.mac_roman.__file__),
+            'aliases.py' + encodings.mac_roman.__file__[:-1])
+
+    imp.reload(encodings.aliases)
+    imp.reload(encodings)
+
+_import_encodings()
