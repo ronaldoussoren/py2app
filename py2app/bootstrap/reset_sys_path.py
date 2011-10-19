39303c5982ea30d3d33a417ef3891f5b41d87769
Ronald Oussoren <ronaldoussoren@mac.com>
2011-10-19 07:55:51 +0200

    Forgot to add a new bootstrap file

---
+++ py2app/bootstrap/reset_sys_path.py
@@ -0,0 +1,7 @@
+def _reset_sys_path():
+    # Clear generic sys.path[0]
+    import sys, os
+    resources = os.environ['RESOURCEPATH']
+    while sys.path[0] == resources:
+        del sys.path[0]
+_reset_sys_path()
