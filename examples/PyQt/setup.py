26f0abcb34263c769109bce42fbdc352719de504
Ronald Oussoren <ronaldoussoren@mac.com>
2011-10-24 10:55:16 +0200

    Remove unwanted unicode text from qt example's setup.py

--- examples/PyQt/setup.py
+++ examples/PyQt/setup.py
@@ -2,11 +2,11 @@
 Script for building the example.
 
 Usage:
-   python setup.py py2app
+    python setup.py py2app
 """
 from distutils.core import setup
 import py2app
 
 setup(
-   app=["hello.py"],
+    app=["hello.py"],
 )
