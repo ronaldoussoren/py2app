4ca35eed5aeed21d8cd0ccad85184d7dfa861d33
Ronald Oussoren <ronaldoussoren@mac.com>
2012-05-02 12:58:56 +0200

    Change syntax of simple example: it is now valid python 3 as wel as python 2

--- examples/simple/hello.py
+++ examples/simple/hello.py
@@ -4,17 +4,17 @@ import pprint
 __version__ = '1.0'
 
 def somefunc():
-    print "Hello from py2app"
+    print ("Hello from py2app")
 
-    print "frozen", repr(getattr(sys, "frozen", None))
+    print ("frozen: %s"%(repr(getattr(sys, "frozen", None)),))
 
     import __main__
-    print __main__.__file__
-    print "sys.path", sys.path
-    print "sys.executable", sys.executable
-    print "sys.prefix", sys.prefix
-    print "sys.argv", sys.argv
-    print "os.getcwd()", os.getcwd()
+    print (__main__.__file__)
+    print ("sys.path: %s"%(sys.path,))
+    print ("sys.executable: %s"%(sys.executable,))
+    print ("sys.prefix: %s"%(sys.prefix,))
+    print ("sys.argv: %s"%(sys.argv,))
+    print ("os.getcwd(): %s"%(os.getcwd(),))
     pprint.pprint(dict(os.environ))
 
 if __name__ == '__main__':
