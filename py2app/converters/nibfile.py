43916e3feabdadbdad63bae19a0a167234b5a21b
Ronald Oussoren <ronaldoussoren@mac.com>
2012-10-09 15:48:17 +0200

    Simplify nib compiler and support recent Xcode versions by using xcrun

--- py2app/converters/nibfile.py
+++ py2app/converters/nibfile.py
@@ -1,31 +1,38 @@
 """
 Automatic compilation of XIB files
 """
+from __future__ import print_function
 import subprocess, os
 from py2app.decorators import converts
+
+gTool = None
+def _get_ibtool():
+    global gTool
+    if gTool is None:
+        if os.path.exists('/usr/bin/xcrun'):
+            gTool = subprocess.check_output(['/usr/bin/xcrun', '-find', 'ibtool'])[:-1]
+        else:
+            gTool = 'ibtool'
+
+    print (gTool)
+    return gTool
 
 @converts(suffix=".xib")
 def convert_xib(source, destination, dry_run=0):
     destination = destination[:-4] + ".nib"
 
+    print("compile %s -> %s"%(source, destination))
     if dry_run:
         return
 
-    p = subprocess.Popen(['ibtool', '--compile', destination, source])
-    xit = p.wait()
-    if xit != 0:
-        raise RuntimeError("ibtool failed, code %d"%(xit,))
-
+    subprocess.check_call([_get_ibtool(), '--compile', destination, source])
 
 @converts(suffix=".nib")
 def convert_nib(source, destination, dry_run=0):
     destination = destination[:-4] + ".nib"
+    print("compile %s -> %s"%(source, destination))
 
     if dry_run:
         return
 
-    p = subprocess.Popen(['ibtool', '--compile', destination, source])
-    xit = p.wait()
-    if xit != 0:
-        raise RuntimeError("ibtool failed, code %d"%(xit,))
-
+    subprocess.check_call([_get_ibtool, '--compile', destination, source])
