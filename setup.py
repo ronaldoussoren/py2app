69f64f7a313f6913edc90250dd6dc47977c9c3c0
Ronald Oussoren <ronaldoussoren@mac.com>
2012-10-22 14:22:53 +0200

    Start of 0.7 branch

--- setup.py
+++ setup.py
@@ -392,7 +392,7 @@ class test (Command):
 setup(
     # metadata
     name='py2app',
-    version='0.7',
+    version='0.7.1',
     description='Create standalone Mac OS X applications with Python',
     #author='Bob Ippolito',
     #author_email='bob@redivi.com',
