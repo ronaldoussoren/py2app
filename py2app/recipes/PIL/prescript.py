bce0587faa3640e69288ee0442e6ad745cce8270
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2020-10-25 10:07:14 +0000

    Fix: AttributeError:  for 'Image.DEBUG'
    AttributeError: module 'PIL.Image' has no attribute 'DEBUG'
    
    DEBUG was removed in: https://github.com/python-pillow/Pillow/pull/1207

--- py2app/recipes/PIL/prescript.py
+++ py2app/recipes/PIL/prescript.py
@@ -37,8 +37,7 @@ def _recipes_pil_prescript(plugins):
 
                 __import__(plugin, globals(), locals(), [])
             except ImportError:
-                if Image.DEBUG:
-                    print("Image: failed to import")
+                print("Image: failed to import")
 
         if Image.OPEN or Image.SAVE:
             Image._initialized = 2
