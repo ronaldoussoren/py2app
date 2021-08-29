6325bc0fb51c46b474ee40f8b36ce2d82a9a4652
Ryan Clary <rclary@trialphaenergy.com>
2021-08-28 21:56:21 -0700

    Add rtree recipe

---
+++ py2app/recipes/rtree.py
@@ -0,0 +1,11 @@
+import os
+
+def check(cmd, mf):
+    m = mf.findNode("rtree")
+    if m is None or m.filename is None:
+        return None
+
+    rtree_dylibs = os.scandir(os.path.join(m.packagepath[0], 'lib'))
+    frameworks = [lib.path for lib in rtree_dylibs]
+
+    return {"frameworks": frameworks}
