9ebaec4b7672bb60a0083c63537e111fa628b43a
Ronald Oussoren <ronaldoussoren@mac.com>
2021-09-27 15:22:57 +0200

    Update version number

--- setup.py
+++ setup.py
@@ -275,7 +275,7 @@ if sys.platform != "darwin":
 setup(
     # metadata
     name="py2app",
-    version="0.26",
+    version="0.26.1",
     description="Create standalone Mac OS X applications with Python",
     # author='Bob Ippolito',
     # author_email='bob@redivi.com',
