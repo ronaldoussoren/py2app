575ad757b0d5a2ce1652b7782420d9eed5ac3fcf
Ronald Oussoren <ronaldoussoren@mac.com>
2022-12-28 15:23:02 +0100

    Sync black recipe with 0.28 branch
    Longer term plan is to move this logic to a
    recipe that detects packages compiled with
    mypyc, but that requires some infrastructure work.

--- src/py2app/recipes/black.py
+++ src/py2app/recipes/black.py
@@ -1,38 +1,75 @@
-import typing
+import ast
+import os
+import sys
 
-try:
-    from importlib.metadata import packages_distributions
-except ImportError:
-    from importlib_metadata import packages_distributions
-
-from modulegraph.modulegraph import ModuleGraph
-
-from .. import build_app
-from ._types import RecipeInfo
+from pkg_resources import get_distribution  # noqa: I251
 
 
-def check(cmd: "build_app.py2app", mf: ModuleGraph) -> typing.Optional[RecipeInfo]:
+def check(cmd, mf):
     m = mf.findNode("black")
     if m is None or m.filename is None:
         return None
 
+    egg = get_distribution("black").egg_info
+    top = os.path.join(egg, "RECORD")
+
     # These cannot be in zip
-    packages = {"black", "blib2to3"}
+    packages = ["black", "blib2to3"]
 
     # black may include optimized platform specific C extension which has
-    # unusual name, e.g. 610faff656c4cfcbb4a3__mypyc; extract
-    # the name from the list of toplevels.
+    # unusual name, e.g. 610faff656c4cfcbb4a3__mypyc; best to determine it from
+    # the egg-info/RECORD file.
+
     includes = set()
-    for toplevel, dists in packages_distributions.items():  # type: ignore
-        if "black" not in dists:
-            continue
+    with open(top) as f:
+        for line in f:
+            fname = line.split(",", 1)[0]
+            toplevel = fname.split("/", 1)[0]
+            if all(ch not in toplevel for ch in (".", "-")):
+                includes.add(toplevel)
 
-        includes.add(toplevel)
+            if fname.endswith(".py"):
+                toplevel_node = mf.findNode(toplevel)
+                if toplevel_node is None:
+                    continue
+                # Black is mypyc compiled, but generally ships with
+                # the original source inside the wheel, that allows  us
+                # to extract dependencies from those sources.
+                #
+                # This is, to state is nicely, a crude hack that uses
+                # internal details of modulegraph.
+                fqname = fname[:-3].replace("/", ".")
+                source_path = os.path.join(os.path.dirname(egg), fname)
+                with open(source_path, "rb") as fp:
+                    update_dependencies_from_source(
+                        mf, fqname, fp, source_path, toplevel_node
+                    )
 
-    includes -= packages
+    includes = list(includes.difference(packages))
 
-    # Missed dependency
-    includes.add("pathspec")
+    return {"includes": includes, "packages": packages}
 
-    # XXX: verify if caller knows how to work with sets
-    return {"includes": list(includes), "packages": sorted(packages)}
+
+def update_dependencies_from_source(mf, fqname, fp, pathname, toplevel):
+    # This logic is from ModuleGraph._load_module, but only
+    # the imp.PY_SOURCE case and without updating the graph
+    m = mf.findNode(fqname)
+    if m is None:
+        m = mf.import_hook(fqname, toplevel)[0]
+
+    contents = fp.read()
+    if isinstance(contents, bytes):
+        contents += b"\n"
+    else:
+        contents += "\n"
+
+    try:
+        co = compile(contents, pathname, "exec", ast.PyCF_ONLY_AST, True)
+        if sys.version_info[:2] == (3, 5):
+            # In Python 3.5 some syntax problems with async
+            # functions are only reported when compiling to bytecode
+            compile(co, "-", "exec", 0, True)
+    except SyntaxError:
+        return
+
+    mf._scan_code(co, m)
