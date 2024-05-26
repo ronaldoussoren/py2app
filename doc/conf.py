9c8ba2efd7b4227545ceb7312318c7d0b644435a
Ronald Oussoren <ronaldoussoren@mac.com>
2024-05-26 09:10:34 +0200

    Fix documentation build issue

--- doc/conf.py
+++ doc/conf.py
@@ -17,7 +17,7 @@ def get_version():
     setup_py = os.path.join(os.path.dirname(
             os.path.dirname(
                 os.path.abspath(__file__))), "setup.py")
-    for ln in open(setup_py, 'rU'):
+    for ln in open(setup_py, 'r'):
         ln = ln.strip()
         if ln.startswith('version='):
             return ln.split("\"")[1]
