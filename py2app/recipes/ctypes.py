3a55dfd35be4be00db18decd9a6a3508be829a13
Ronald Oussoren <ronaldoussoren@mac.com>
2025-11-23 22:27:21 +0100

    Workaround for including ctypes on Python 3.13+

--- py2app/recipes/ctypes.py
+++ py2app/recipes/ctypes.py
@@ -1,7 +1,11 @@
+import sys
 def check(cmd, mf):
     print("CTYPES USERS", list(mf.getReferers("ctypes")))
     m = mf.findNode("ctypes")
     if m is None or m.filename is None:
         return None
 
-    return {"prescripts": ["py2app.bootstrap.ctypes_setup"]}
+    if sys.version_info[:2] >= (3, 13):
+        return {"prescripts": ["py2app.bootstrap.ctypes_setup"], "packages": ["ctypes"]}
+    else:
+        return {"prescripts": ["py2app.bootstrap.ctypes_setup"]}
